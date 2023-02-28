68c225e0147655fa1a65039c09abb57b03c2405e
srhime <himusr@gmail.com>
2023-02-28 15:09:18 +0600

    added mongostring to scheduler

--- apiScheduler.js
+++ apiScheduler.js
@@ -1,6 +1,7 @@
 const { default: axios } = require('axios')
 const { Schema, model, models } = require('mongoose')
 const schedule = require('node-schedule')
+const mongoose = require('mongoose')
 
 const apiResponseSchema = new Schema({
   response: {
@@ -11,8 +12,13 @@ const apiResponseSchema = new Schema({
 const ApiResponse =
   models.ApiResponse || model('ApiResponse', apiResponseSchema)
 
+const connectMongo = async () => {
+  await mongoose.connect(process.env.MONGO_URI)
+}
+
 exports.apiScheduler = schedule.scheduleJob('0 */8 * * *', async () => {
   try {
+    await connectMongo()
     const response = await axios.get(
       `http://api.coinlayer.com/live?access_key=${process.env.COINLAYER}`
     )
